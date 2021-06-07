bc06a0959238c6c7e9629d56893b13ebe1fdf226
lxie-0709 <85497179+lxie-0709@users.noreply.github.com>
2021-06-07 21:17:15 +0800

    Update 21_24_filter.py

--- 21_24_filter.py
+++ 21_24_filter.py
@@ -1,17 +1,17 @@
 import sys
 sam=open(sys.argv[1],"r")
-out=open(sys.argv[2],"w")
-out1=open(sys.argv[3],"w")
+out_24nt=open(sys.argv[2],"w")
+out_21nt=open(sys.argv[3],"w")
 for i in sam:
-	a=i.strip().split("\t")
-	if len(a)>6:
-		ll="\t".join(a)
-		if a[5].split("M")[0]=="24":
-			out.write(ll+"\n")
-		if a[5].split("M")[0]=="21":
-			out1.write(ll+"\n")
+	lines=i.strip().split("\t")
+	if len(lines)>6:
+		ll="\t".join(lines)
+		if lines[5].split("M")[0]=="24":
+			out_24nt.write(ll+"\n")
+		if lines[5].split("M")[0]=="21":
+			out_21nt.write(ll+"\n")
 	else:
-		ll="\t".join(a)
+		ll="\t".join(lines)
 		out1.write(ll+"\n")
 		out.write(ll+"\n")
 sam.close()
