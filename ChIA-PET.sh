1a7b2b963a35bb910c25bbc42ddd7f501d7e4af4
lxie-0709 <85497179+lxie-0709@users.noreply.github.com>
2021-06-18 21:02:32 +0800

    Update ChIA-PET.sh

--- ChIA-PET.sh
+++ ChIA-PET.sh
@@ -4,5 +4,5 @@
 for sample in *_1.fastq;do
 index="BWA_IDX_PATH"
 genomesize="GENOME_SIZE"
-ChIA-PET2 -g $index -b $genomesize -f $sample -r ${sample%%_*}_2.fastq  -A linkerA -B linkerB -o OUTdir -n name
+java -jar ChIA-PET.jar --mode 1 --fastq1 test_long_1.fastq --fastq2 test_long_2.fastq --linker ChIA-PET_Tool_V3/linker/linker_long.txt --minimum_linker_alignment_score 14 --GENOME_INDEX $index --GENOME_LENGTH 3.6E8 --CHROM_SIZE_INFO $genomesize --CYTOBAND_DATA oryza_cytoBandIdeo.txt --output test_long --prefix OUT --thread 4
 done
